87f3afae665605969c30639f73cf77e641c255ad
um21 <ujjwal.mishra@getfitso.com>
2020-06-18 13:40:42 +0530

    prod url added to axios

--- src/axios-order.js
+++ src/axios-order.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL:'http://localhost:3050/'
+    baseURL:'https://spotbook101.herokuapp.com/'
 });
 
 export default instance;
