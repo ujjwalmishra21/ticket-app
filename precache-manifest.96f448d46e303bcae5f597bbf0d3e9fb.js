ac2210bde0a9f092868528ec80cf753a3f8e8288
um21 <ujjwal.mishra@getfitso.com>
2020-06-18 13:57:38 +0530

    Updates

---
+++ precache-manifest.96f448d46e303bcae5f597bbf0d3e9fb.js
@@ -0,0 +1,174 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02ea32fd0db972c4bfdb6b5fac5aa6e6",
+    "url": "/ticket-app/index.html"
+  },
+  {
+    "revision": "6f83ffbf2a4d24cf031a",
+    "url": "/ticket-app/static/css/10.055c2566.chunk.css"
+  },
+  {
+    "revision": "a04496714679efdeed06",
+    "url": "/ticket-app/static/css/11.62f7bdf2.chunk.css"
+  },
+  {
+    "revision": "fe467f42a19e7ee3bd51",
+    "url": "/ticket-app/static/css/12.bda048a5.chunk.css"
+  },
+  {
+    "revision": "869a8654d468ce15680b",
+    "url": "/ticket-app/static/css/6.0d02f2c8.chunk.css"
+  },
+  {
+    "revision": "84037680071570fcf5ff",
+    "url": "/ticket-app/static/css/9.055c2566.chunk.css"
+  },
+  {
+    "revision": "f75994a70449c4d02bc4",
+    "url": "/ticket-app/static/css/main.b01ee141.chunk.css"
+  },
+  {
+    "revision": "8bf6ee1ba03c15a06e7a",
+    "url": "/ticket-app/static/js/0.3f59c233.chunk.js"
+  },
+  {
+    "revision": "97636669c8193ace9a3b4f1548fc4d0b",
+    "url": "/ticket-app/static/js/0.3f59c233.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c29eb99367aaed131d3c",
+    "url": "/ticket-app/static/js/1.f58cf3d1.chunk.js"
+  },
+  {
+    "revision": "6f83ffbf2a4d24cf031a",
+    "url": "/ticket-app/static/js/10.e5341bba.chunk.js"
+  },
+  {
+    "revision": "a04496714679efdeed06",
+    "url": "/ticket-app/static/js/11.493db2c0.chunk.js"
+  },
+  {
+    "revision": "fe467f42a19e7ee3bd51",
+    "url": "/ticket-app/static/js/12.80e845af.chunk.js"
+  },
+  {
+    "revision": "cf9a364959015583ba89",
+    "url": "/ticket-app/static/js/13.e98430fc.chunk.js"
+  },
+  {
+    "revision": "3517d063aa002699f24d",
+    "url": "/ticket-app/static/js/14.f2027034.chunk.js"
+  },
+  {
+    "revision": "9082cb6ecc440233a281",
+    "url": "/ticket-app/static/js/15.c2611764.chunk.js"
+  },
+  {
+    "revision": "f26792863d2b24505652",
+    "url": "/ticket-app/static/js/16.f598f1ba.chunk.js"
+  },
+  {
+    "revision": "886b61c062f2d5db5ef3",
+    "url": "/ticket-app/static/js/2.8ad1808a.chunk.js"
+  },
+  {
+    "revision": "dd673aacd0266eb808be",
+    "url": "/ticket-app/static/js/3.f9e5f3ba.chunk.js"
+  },
+  {
+    "revision": "869a8654d468ce15680b",
+    "url": "/ticket-app/static/js/6.33fec675.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/ticket-app/static/js/6.33fec675.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1d2538cb44647315d7eb",
+    "url": "/ticket-app/static/js/7.53fe2939.chunk.js"
+  },
+  {
+    "revision": "000d6c36287224a7ff48f88e64636454",
+    "url": "/ticket-app/static/js/7.53fe2939.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fa8040f795b66d753a4d",
+    "url": "/ticket-app/static/js/8.04c8f32b.chunk.js"
+  },
+  {
+    "revision": "84037680071570fcf5ff",
+    "url": "/ticket-app/static/js/9.b88ce0aa.chunk.js"
+  },
+  {
+    "revision": "f75994a70449c4d02bc4",
+    "url": "/ticket-app/static/js/main.49f6b3f4.chunk.js"
+  },
+  {
+    "revision": "2bf977cf7c5981814ba3",
+    "url": "/ticket-app/static/js/runtime-main.f5b5bf22.js"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/ticket-app/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/ticket-app/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/ticket-app/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/ticket-app/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/ticket-app/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/ticket-app/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/ticket-app/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/ticket-app/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/ticket-app/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/ticket-app/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/ticket-app/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/ticket-app/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/ticket-app/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/ticket-app/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/ticket-app/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/ticket-app/static/media/outline-icons.ef60a4f6.woff"
+  }
+]);
